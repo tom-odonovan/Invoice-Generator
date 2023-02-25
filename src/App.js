af4cbee24237e1b99d1daf676179349fd0536e2b
Tom O'Donovan <tom.odonovan01@gmail.com>
2023-02-25 18:14:32 +1100

    Create inputs for client details in Form.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,55 @@ import Preview from './components/Preview';
 function App() {
   const [preview, setPreview] = useState(false)
 
+  const [invoiceDetails, setInvoiceDetails] = useState({})
+  const [vendorDetails, setVendorDetails] = useState({})
+  const [clientDetails, setClientDetails] = useState({})
+  const [paymentDetails, setPaymentDetails] = useState({})
+  const [contactInfo, setContactInfo] = useState({})
+  const [notes, setNotes] = useState({})
+
+  const setName = (e) => {
+    setClientDetails((prevState) => ({
+      ...prevState, 
+      name: e.target.value 
+    }));
+  }
+
+  const setEmail = (e) => {
+    setClientDetails((prevState) => ({
+      ...prevState,
+      email: e.target.value
+    }));
+  }
+
+  const setAddress = (e) => {
+    setClientDetails((prevState) => ({
+      ...prevState,
+      address: e.target.value
+    }));
+  }
+
+  const setSuburb = (e) => {
+    setClientDetails((prevState) => ({
+      ...prevState,
+      suburb: e.target.value
+    }));
+  }
+
+  const setStateTerritory = (e) => {
+    setClientDetails((prevState) => ({
+      ...prevState,
+      stateTerritory: e.target.value
+    }));
+  }
+
+  const setPostcode = (e) => {
+    setClientDetails((prevState) => ({
+      ...prevState,
+      postcode: e.target.value
+    }));
+  }
+
   const showPreview = () => {
     setPreview(true)
   }
@@ -23,7 +72,23 @@ function App() {
         {preview ? 
           <Preview hidePreview={hidePreview} />
         : (
-          <Form showPreview={showPreview} />
+          <Form
+            name={clientDetails.name}
+            handleName={setName}
+            email={clientDetails.email}
+            handleEmail={setEmail}
+            address={clientDetails.address}
+            handleAddress={setAddress}
+            suburb={clientDetails.suburb}
+            handleSuburb={setSuburb}
+            stateTerritory={clientDetails.stateTerritory}
+            handleStateTerritory={setStateTerritory}
+            postcode={clientDetails.postcode}
+            handlePostcode={setPostcode}
+
+            showPreview={showPreview}
+            details={clientDetails}
+           />
         )}
         
       </main>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,97 @@
 import React from 'react'
 import '../css/Form.css'
 
-export default function Form({ showPreview }) {
+export default function Form(props) {
+
+  const { name, handleName, 
+          email, handleEmail,
+          address, handleAddress,
+          suburb, handleSuburb,
+          stateTerritory, handleStateTerritory,
+          postcode, handlePostcode,
+          showPreview, details } = props
+
+  const handleLogDetails = () => {
+    console.log(details);
+  };
+
   return (
     <div>
-        <div className='template-form'>
-            <input type='text' placeholder='Enter your name'></input>
-            <button 
-              className='preview-btn'
-              onClick={showPreview}
-            >Preview Invoice</button>
-        </div>
+        <form className='template-form'>
+          <label htmlFor='name'>Full Name</label>
+          <input 
+            type='text'
+            name='name'
+            required 
+            placeholder='Enter your name'
+            value={name || ''}
+            onChange={handleName}
+          ></input>
+
+          <label htmlFor='email'>Email address</label>
+          <input 
+            type='email'
+            name='email' 
+            placeholder='Enter your email'
+            value={email || ''}
+            onChange={handleEmail}
+          ></input>
+
+          <label htmlFor='email'>Address</label>
+          <input 
+            type='text'
+            name='address' 
+            placeholder='Enter your address'
+            value={address || ''}
+            onChange={handleAddress}
+          ></input>
+
+          <label htmlFor='email'>Suburb</label>
+          <input 
+            type='text'
+            name='suburb' 
+            placeholder='Suburb'
+            value={suburb || ''}
+            onChange={handleSuburb}
+          ></input>
+
+          <label htmlFor='email'>State/territory</label>
+          <select 
+            name='State' 
+            value={stateTerritory || ''}
+            onChange={handleStateTerritory}
+          >
+            <option value=''>Select State/territory</option>
+            <option value='ACT'>ACT</option>
+            <option value='NSW'>NSW</option>
+            <option value='NT'>NT</option>
+            <option value='QLD'>QLD</option>
+            <option value='SA'>SA</option>
+            <option value='TAS'>TAS</option>
+            <option value='VIC'>VIC</option>
+            <option value='WA'>WA</option>
+          </select>
+
+          <label htmlFor='email'>Postcode</label>
+          <input 
+            type='text'
+            name='postcode' 
+            placeholder='Postcode'
+            value={postcode || ''}
+            onChange={handlePostcode}
+          ></input>
+
+          <button 
+            className='preview-btn'
+            onClick={showPreview}
+          >Preview Invoice</button>
+
+          <button 
+            className='preview-btn'
+            type='button'
+            onClick={handleLogDetails}
+          >Log details</button>
+        </form>
     </div>
   )
 }
