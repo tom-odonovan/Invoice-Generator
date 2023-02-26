efbfec724ce5724eecf3e293194e37b8537d324d
Tom O'Donovan <tom.odonovan01@gmail.com>
2023-02-26 12:55:57 +1100

    Basic form styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -20,126 +20,148 @@ export default function Form(props) {
 
           {/* INVOICE DETAILS */}
 
-          <label htmlFor='invoiceNo'>Invoice number</label>
-          <input 
-            type='text'
-            name='number'
-            required 
-            placeholder='Invoice number'
-            value={invoiceDetails.number || ''}
-            onChange={handleInvoiceDetails}
-          ></input>
+          <div className='form-invoice-details'>
+            <div>
+              <label htmlFor='invoiceNo'>Invoice number</label>
+              <input 
+                type='text'
+                name='number'
+                required 
+                placeholder='Invoice number'
+                value={invoiceDetails.number || ''}
+                onChange={handleInvoiceDetails}
+              ></input>
+            </div>
 
-          <label htmlFor='date'>Invoice date</label>
-          <input 
-            type='date'
-            name='date'
-            required 
-            placeholder='Invoice date'
-            value={invoiceDetails.date || ''}
-            onChange={handleInvoiceDetails}
-          ></input>
+            <div>
+              <label htmlFor='date'>Invoice date</label>
+              <input 
+                type='date'
+                name='date'
+                required 
+                placeholder='Invoice date'
+              value={invoiceDetails.date || new Date().toLocaleDateString('en-CA')}
+                onChange={handleInvoiceDetails}
+              ></input>
+            </div>
 
-          <label htmlFor='invoiceNo'>Due date</label>
-          <input 
-            type='date'
-            name='dueDate'
-            required 
-            placeholder='Due date'
-            value={invoiceDetails.dueDate || ''}
-            onChange={handleInvoiceDetails}
-          ></input>
+            <div>
+              <label htmlFor='invoiceNo'>Due date</label>
+              <input 
+                type='date'
+                name='dueDate'
+                required 
+                placeholder='Due date'
+                value={invoiceDetails.dueDate || ''}
+                onChange={handleInvoiceDetails}
+              ></input>
+            </div>
+          </div>
 
           {/* VENDOR DETAILS */}
 
-          <label htmlFor='name'>Your Full Name</label>
-          <input 
-            type='text'
-            name='name'
-            required 
-            placeholder='Enter your name'
-            value={vendorDetails.name || ''}
-            onChange={handleVendorDetails}
-          ></input>
+          <h3>Personal Details</h3>
 
-          <label htmlFor='email'>Email address</label>
-          <input 
-            type='email'
-            name='email'
-            required
-            placeholder='Enter your email'
-            value={vendorDetails.email || ''}
-            onChange={handleVendorDetails}
-          ></input>
+          <div className='form-vendor-details'>
+            <label htmlFor='name'>Your Full Name</label>
+            <input 
+              type='text'
+              name='name'
+              required 
+              placeholder='Enter your name'
+              value={vendorDetails.name || ''}
+              onChange={handleVendorDetails}
+            ></input>
 
-          <label htmlFor='address'>Address</label>
-          <input 
-            type='text'
-            name='address'
-            required 
-            placeholder='Enter your address'
-            value={vendorDetails.address || ''}
-            onChange={handleVendorDetails}
-          ></input>
+            <label htmlFor='email'>Email address</label>
+            <input 
+              type='email'
+              name='email'
+              required
+              placeholder='Enter your email'
+              value={vendorDetails.email || ''}
+              onChange={handleVendorDetails}
+            ></input>
 
-          <label htmlFor='suburb'>Suburb</label>
-          <input 
-            type='text'
-            name='suburb'
-            required 
-            placeholder='Suburb'
-            value={vendorDetails.suburb || ''}
-            onChange={handleVendorDetails}
-          ></input>
+            <label htmlFor='address'>Address</label>
+            <input 
+              type='text'
+              name='address'
+              required 
+              placeholder='Enter your address'
+              value={vendorDetails.address || ''}
+              onChange={handleVendorDetails}
+            ></input>
 
-          <label htmlFor='state'>State/territory</label>
-          <select 
-            name='state'
-            required 
-            value={vendorDetails.state || ''}
-            onChange={handleVendorDetails}
-          >
-            <option value=''>Select State/territory</option>
-            <option value='ACT'>ACT</option>
-            <option value='NSW'>NSW</option>
-            <option value='NT'>NT</option>
-            <option value='QLD'>QLD</option>
-            <option value='SA'>SA</option>
-            <option value='TAS'>TAS</option>
-            <option value='VIC'>VIC</option>
-            <option value='WA'>WA</option>
-          </select>
+            <div className='address-ln-2'>
+              <div>
+                <label htmlFor='suburb'>Suburb</label>
+                <input 
+                  type='text'
+                  name='suburb'
+                  required 
+                  placeholder='Suburb'
+                  value={vendorDetails.suburb || ''}
+                  onChange={handleVendorDetails}
+                ></input>
+              </div>
 
-          <label htmlFor='postcode'>Postcode</label>
-          <input 
-            type='text'
-            name='postcode'
-            required 
-            placeholder='Postcode'
-            value={vendorDetails.postcode || ''}
-            onChange={handleVendorDetails}
-          ></input>
+              <div>
+                <label htmlFor='state'>State/territory</label>
+                <select 
+                  name='state'
+                  required 
+                  value={vendorDetails.state || ''}
+                  onChange={handleVendorDetails}
+                >
+                  <option value=''>Select State/territory</option>
+                  <option value='ACT'>ACT</option>
+                  <option value='NSW'>NSW</option>
+                  <option value='NT'>NT</option>
+                  <option value='QLD'>QLD</option>
+                  <option value='SA'>SA</option>
+                  <option value='TAS'>TAS</option>
+                  <option value='VIC'>VIC</option>
+                  <option value='WA'>WA</option>
+                </select>
+              </div>
 
-          <label htmlFor='phone'>Phone number</label>
-          <input 
-            type='text'
-            name='phone' 
-            placeholder='Enter your phone number'
-            value={vendorDetails.phone || ''}
-            onChange={handleVendorDetails}
-          ></input>
+              <div>
+                <label htmlFor='postcode'>Postcode</label>
+                <input 
+                  type='text'
+                  name='postcode'
+                  required 
+                  placeholder='Postcode'
+                  value={vendorDetails.postcode || ''}
+                  onChange={handleVendorDetails}
+                ></input>
+              </div>
+            </div>
 
-          <label htmlFor='website'>Website (optional)</label>
-          <input 
-            type='text'
-            name='website' 
-            placeholder='Your website url'
-            value={vendorDetails.website || ''}
-            onChange={handleVendorDetails}
-          ></input>
+            <label htmlFor='phone'>Phone number</label>
+            <input 
+              type='text'
+              name='phone' 
+              placeholder='Enter your phone number'
+              value={vendorDetails.phone || ''}
+              onChange={handleVendorDetails}
+            ></input>
+
+            <label htmlFor='website'>Website (optional)</label>
+            <input 
+              type='text'
+              name='website' 
+              placeholder='Your website url'
+              value={vendorDetails.website || ''}
+              onChange={handleVendorDetails}
+            ></input>
+          </div>
 
 
           {/* CLIENT DETAILS */}
+
+          <h3>Client Details</h3>
 
           <label htmlFor='clientName'>Client's Name</label>
           <input 
