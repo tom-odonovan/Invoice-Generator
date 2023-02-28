1991ad5034739df1fb0a692396cc725942f3df6d
Tom O'Donovan <tom.odonovan01@gmail.com>
2023-02-28 22:45:42 +1100

    Create a new form component for adding invoice items

--- src/components/ItemSummary.js
+++ src/components/ItemSummary.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import '../css/ItemSummary.css'
 
-export default function ItemSummary() {
+export default function ItemSummary(props) {
+
+  const { description, quantity, price, total } = props
+
   return (
     <div>
-        <section>
-          {/* Table */}
-          <p>Insert table here</p>
-        </section>
+        <table className='item-summary'>
+          <thead>
+            <tr>
+              <td>Item Description</td>
+              <td>Quantity</td>
+              <td>Price</td>
+              <td>Total</td>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>{ description }</td>
+              <td>{ quantity }</td>
+              <td>{ price }</td>
+              <td>{ total }</td>
+            </tr>
+          </tbody>
+        </table>
     </div>
   )
 }
