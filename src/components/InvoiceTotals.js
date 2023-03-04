fd3e066d3fdf110f113e9f1ed87703440a4ec82e
Tom O'Donovan <tom.odonovan01@gmail.com>
2023-03-04 14:05:06 +1100

    Add component to display invoice totals

---
+++ src/components/InvoiceTotals.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../css/InvoiceTotals.css'
+
+export default function InvoiceTotals() {
+
+    // const { subTotal, tax, discount, total } = invoiceTotals
+
+    return (
+        <div className='invoice-totals-cont'>
+            <div className='invoice-totals'>
+                <ul>
+                    <li><b>Subtotal:</b>$0.00</li>
+                    <li><b>Tax:</b>$0.00</li>
+                    <li><b>Discount:</b>$0.00</li>
+                </ul>
+                <div>
+                    <p><b>Total:</b>$0.00</p>
+                </div>
+            </div>
+        </div>
+    )
+}
