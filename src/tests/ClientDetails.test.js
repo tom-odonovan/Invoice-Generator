bbb5113e6e8903b00ba627fd8d1cbd8610c72abc
Tom O'Donovan <tom.odonovan01@gmail.com>
2023-03-05 21:08:30 +1100

    Create separate folder for tests

--- src/tests/ClientDetails.test.js
+++ src/tests/ClientDetails.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import ClientDetails from './ClientDetails';
+import ClientDetails from '../components/ClientDetails';
 
 const clientDetails = {
     name: 'XYZ Corporation',
